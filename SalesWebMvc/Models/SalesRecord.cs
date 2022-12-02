9610beea92f56f5aac46cd5c116d570d128e0dd9
Rafael Jáber <rafael.jaber@hotmail.com>
2022-12-02 00:34:11 -0300

    Final tweaks to the application layout

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -3,9 +3,13 @@ using SalesWebMvc.Models.Enums;
 
 namespace SalesWebMvc.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class SalesRecord : BaseModel
     {
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
